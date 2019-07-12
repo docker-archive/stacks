bf4deef51884f8d01981149d942f1692f8eabbaa
Jason Schroeder <jason_schroeder@docker.com>
2019-07-12 15:55:22 +0000

    Removing kubernetes code.  Removing commented out tests.

--- pkg/router/router_test.go
+++ pkg/router/router_test.go
@@ -7,7 +7,6 @@ import (
 	"github.com/docker/docker/api/types/swarm"
 	"github.com/docker/docker/errdefs"
 	"github.com/stretchr/testify/require"
-	"gotest.tools/assert"
 
 	"github.com/docker/stacks/pkg/client/fake"
 
@@ -94,102 +93,4 @@ func TestRouterMultipleBackendsUpdate(t *testing.T) {
 	require.Error(err)
 	require.Contains(err.Error(), "update out of sequence")
 
-}
-
-func TestRouterMultipleBackendsCRD(t *testing.T) {
-	require := require.New(t)
-	ctx := context.Background()
-
-	router := NewStacksRouter()
-	swarmBackend := fake.NewStackClient()
-	kubeBackend := fake.NewStackClient(fake.WithStartingID(5000)) // Set a separate starting ID to avoid ID conflicts
-
-	router.RegisterBackend(types.OrchestratorSwarm, swarmBackend)
-	router.RegisterBackend(types.OrchestratorKubernetes, kubeBackend)
-
-	// Create a swarm stack.
-	swarmResp, err := router.StackCreate(ctx, swarmStackCreate, types.StackCreateOptions{})
-	require.NoError(err)
-	require.NotEmpty(swarmResp.ID)
-
-	// Ensure it's created via the router API.
-	stack, err := router.StackInspect(ctx, swarmResp.ID)
-	require.NoError(err)
-	assert.DeepEqual(t, stack.Spec, swarmStackCreate)
-	require.Equal(stack.ID, swarmResp.ID)
-
-	stacks, err := router.StackList(ctx, types.StackListOptions{})
-	require.NoError(err)
-	assert.DeepEqual(t, stacks[0].Spec, swarmStackCreate)
-	require.Equal(stacks[0].ID, swarmResp.ID)
-
-	// Ensure the swarm stack only shows up on the swarm backend.
-	stack, err = swarmBackend.StackInspect(ctx, swarmResp.ID)
-	require.NoError(err)
-	assert.DeepEqual(t, stack.Spec, swarmStackCreate)
-	require.Equal(stacks[0].ID, swarmResp.ID)
-
-	// stack, err = kubeBackend.StackInspect(ctx, swarmResp.ID)
-	// require.Error(err)
-	// require.True(errdefs.IsNotFound(err))
-	// require.Empty(stack)
-
-	// // Now create a kube stack.
-	// kubeResp, err := router.StackCreate(ctx, kubeStackCreate, types.StackCreateOptions{})
-	// require.NoError(err)
-	// require.NotEmpty(kubeResp.ID)
-
-	// // Ensure the kube stack only shows up on the kube backend.
-	// stack, err = kubeBackend.StackInspect(ctx, kubeResp.ID)
-	// require.NoError(err)
-	// assert.DeepEqual(t, stack.Spec, kubeStackCreate)
-	// require.Equal(stack.ID, kubeResp.ID)
-
-	// stack, err = swarmBackend.StackInspect(ctx, kubeResp.ID)
-	// require.Error(err)
-	// require.True(errdefs.IsNotFound(err))
-	// require.Empty(stack)
-
-	// // We should be able to list both stacks through the router.
-	// stacks, err = router.StackList(ctx, types.StackListOptions{})
-	// require.NoError(err)
-	// require.Len(stacks, 2)
-
-	// looking := map[string]types.StackSpec{
-	// 	swarmResp.ID: swarmStackCreate,
-	// 	kubeResp.ID:  kubeStackCreate,
-	// }
-
-	// for _, stack := range stacks {
-	// 	create, ok := looking[stack.ID]
-	// 	require.True(ok)
-	// 	assert.DeepEqual(t, stack.Spec, create)
-	// 	delete(looking, stack.ID)
-	// }
-	// require.Empty(looking)
-
-	// // Delete the kube stack via the router.
-	// require.NoError(router.StackDelete(ctx, kubeResp.ID))
-
-	// // Ensure the kube stack was removed
-	// stacks, err = router.StackList(ctx, types.StackListOptions{})
-	// require.NoError(err)
-	// require.Len(stacks, 1)
-	// require.Equal(stacks[0].ID, swarmResp.ID)
-
-	// // The stack does not exist on any backend at this point
-	// stack, err = router.StackInspect(ctx, kubeResp.ID)
-	// require.Error(err)
-	// require.True(errdefs.IsNotFound(err))
-	// require.Empty(stack)
-
-	// stack, err = kubeBackend.StackInspect(ctx, kubeResp.ID)
-	// require.Error(err)
-	// require.True(errdefs.IsNotFound(err))
-	// require.Empty(stack)
-
-	// stack, err = swarmBackend.StackInspect(ctx, kubeResp.ID)
-	// require.Error(err)
-	// require.True(errdefs.IsNotFound(err))
-	// require.Empty(stack)
 }
